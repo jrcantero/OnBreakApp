b6ef976d4ddb22874f9ae25b78cc253410fd64b3
Jorge Cantero <canteroster@gmail.com>
2019-04-08 21:13:49 -0400

    Agregar archivos de proyecto.

---
+++ OnBreak/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBreak
+{
+    class Cliente
+    {
+    }
+
+    public enum _tipoEmpresa
+    {
+        SPA,
+        EIRL,
+        Limitada,
+        Sociedad_Anonima
+    }
+
+    public enum _actividadEmpresa
+    {
+        Agropecuaria,
+        Mineria,
+        Manufactura,
+        Comercio,
+        Hoteleria,
+        Alimentos,
+        Transporte,
+        Servicios
+    }
+}
